9b24a8d60bc8612b755f60513416a9120da84cf7
maximiliann97 <maximilian@live.se>
2022-10-08 18:43:13 +0200

    InitializePopulation

--- HP2/Problem 2.4/InitializePopulation.m
+++ HP2/Problem 2.4/InitializePopulation.m
@@ -1,13 +1,27 @@
-function population = InitializePopulation(populationSize,instructionRange,nVariableRegisters,nConstantRegisters)
+function population = InitializePopulation(populationSize,instructionRange,nVariableRegisters,nConstantRegisters,operatorSet)
     
-
+    nOperands = nVariableRegisters + nConstantRegisters;
+    nOperators = length(operatorSet);
     population = [];
     for i = 1:populationSize
-        numberOfInstructions = instructionRange(1) - fix(rand*(instructionRange(2)-instructionRange(1)+1));
-        chromosome = zeros(1, numberOfInstructions*4);
+        numberOfInstructions = randi(instructionRange);
+        chromosome = zeros(1,numberOfInstructions*4);
 
         for j = 1:numberOfInstructions
+            % Each instruction consists of 4 instances
+            iInstruction = 1 + (j-1)*4;
             
+            % Instruction instances are created
+            % Order: Operator,destination register, operand 1, operand 2
+            chromosome(iInstruction) = randi(nOperators);
+            chromosome(iInstruction+1) = randi(nVariableRegisters);
+            chromosome(iInstruction+2) = randi(nOperands);
+            chromosome(iInstruction+3) = randi(nOperands);
+        end
+        individual = struct('Chromosome',chromosome);
+        population = [population individual];
+    end
+
     
 end
 
